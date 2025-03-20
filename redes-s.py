4e69b12739c1c2781144c9410f5872cffccc0963
davayme <dayme1937@uta.edu.ec>
2025-03-20 12:09:16 -0500

    Agregar nodos de profesiones y habilidades, y establecer relaciones en redes-s.py

--- redes-s.py
+++ redes-s.py
@@ -1,10 +1,28 @@
-#redes semanticas 20-03-2025
 import networkx as nx
 import matplotlib.pyplot as plt
 
 G = nx.DiGraph()
-G.add_edges_from([('mamifero', 'animal'), ('gato', 'mamifero'), ('perro', 'mamifero')])
 
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True, node_size=2500, node_color='orange', font_size=20, font_color='black', font_weight='bold', edge_color='blue', width=3, style='dashed')
+#Agregar nodos de profesiones y habilidades
+profesiones = ["Ingeniero", "Doctor", "Programador", "Cientifico de Datos"] 
+habilidades = ["Matematicas", "Programacion", "Analisis de Datos", "Medicina"]
+
+G.add_nodes_from(profesiones, color='blue')
+G.add_nodes_from(habilidades, color='green')
+
+#Agregar las relaciones 
+
+G.add_edges_from([("Ingeniero", "Matematicas"), 
+                  ("Programador", "Programacion"), 
+                  ("Cientifico de Datos", "Analisis de Datos"),
+                  ("Doctor", "Medicina"),
+                  #("Doctor", "Analisis de Datos"),
+                  ("Cientifico de Datos", "Programacion"),
+                  #("Cientifico de Datos", "Matematicas"),
+                  ("Ingeniero", "Analisis de Datos"), ])
+
+#Dibujar el grafo
+plt.figure(figsize=(7, 5))
+nx.draw(G, with_labels=True, node_color="lightblue", edge_color='gray', node_size=2500, font_size=9)
+
 plt.show()
